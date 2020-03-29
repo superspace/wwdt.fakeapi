1a5d6bb9f987e80f46b1bfa429c65d51c71cdd8a
Urs Beyeler <dev@superspace.ch>
2020-03-29 18:45:23 +0200

    Refernce markers by id

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL & ~E_NOTICE);
 
 header("Content-type:application/json");
 header("Access-Control-Allow-Origin: *");
@@ -12,28 +14,85 @@ if (isset($_REQUEST['q'])) {
         'status'=>'ERROR'
     );
 
+
+
     switch ($endpoint) {
 
-        case 'list-projects' :
+        // Get
+
+        case 'get-projects' :
             $data = file_get_contents('data/projects.json');
             $response['result'] = json_decode($data);
             $response['status'] = 'OK';
             break;
 
-        case 'list-markers' :
-            $data = file_get_contents('data/markers.json');
-            $response['result'] = json_decode($data);
-            $response['status'] = 'OK';
+        case 'get-assets' :
+            $id = isset($_GET['sessionId']) ? $_GET['sessionId'] : false;
+            if ($id) {
+                $data = file_get_contents('data/assets.json');
+                $response['result'] = json_decode($data);
+                $response['status'] = 'OK';
+            } else {
+                $response['message'] = 'ID missing';
+            }
             break;
 
-        case 'add-marker' :
+        case 'get-markers' :
+            $id = isset($_GET['sessionId']) ? $_GET['sessionId'] : false;
+            if ($id) {
+                $data = file_get_contents('data/markers.json');
+                $response['result'] = json_decode($data);
+                $response['status'] = 'OK';
+            } else {
+                $response['message'] = 'ID missing';
+            }
+            break;
+
+        case 'get-arrangement' :
+            $id = isset($_GET['id']) ? $_GET['id'] : false;
+            if ($id) {
+                $data = file_get_contents('data/arrangement.json');
+                $response['result'] = json_decode($data);
+                $response['status'] = 'OK';
+            } else {
+                $response['message'] = 'ID missing';
+            }
+            break;
+
+        // Create
+
+        case 'create-marker' :
             $response['status'] = 'OK';
             $response['params'] = json_decode(file_get_contents('php://input'));
             break;
 
-        case 'add-asset' :
+        case 'create-asset' :
             $response['status'] = 'OK';
-            $response['params'] = json_decode(file_get_contents('php://input'));
+            $response['request'] = json_decode(file_get_contents('php://input'));
+            break;
+
+        /**
+         * Update Asset
+         * 
+         * @param id
+         * @param title
+         * @param description
+         **/
+        
+        case 'update-asset' :
+            $response['status'] = 'OK';
+            $response['request'] = $_POST;            
+            break;
+
+        /**
+         * Delete Asset
+         * 
+         * @param id
+         **/
+
+        case 'delete-asset' :
+            $response['status'] = 'OK';
+            $response['request'] = $_POST;            
             break;
 
         default :
